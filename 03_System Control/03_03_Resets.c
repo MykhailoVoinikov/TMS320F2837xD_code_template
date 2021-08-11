4d23a610ac0bc3fa22de8b0b574547460c771907
Mykhailo <48632891+MykhailoVoinikov@users.noreply.github.com>
2021-08-11 20:44:36 +0200

    reset sources

--- 03_System Control/03_03_Resets.c
+++ 03_System Control/03_03_Resets.c
@@ -1,1 +1,22 @@
 #include "03_03_Resets.h"
+
+
+void chiplevelResets (void)
+{
+	
+}
+
+void CPU1_SystemResets (void)
+{
+	
+}
+
+void CPU2_SubsystemResets (void)
+{
+	
+}
+
+void specialResets (void)
+{
+	
+}
--- 03_System Control/03_03_Resets.h
+++ 03_System Control/03_03_Resets.h
@@ -1,7 +1,26 @@
-#ifndef 3_3_RESETS_
-#define 3_3_RESETS_
+#ifndef _3_3_RESETS_
+#define _3_3_RESETS_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_nmi.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_types.h"
+#include "cpu.h"
+#include "debug.h"
+#include "interrupt.h"
+#include "sysctl.h"
 
 //Reset Sources
+void chiplevelResets (void);
+void CPU1_SystemResets (void);
+void CPU2_SubsystemResets (void);
+void specialResets (void);
+
+//read RESC by SysCtl_getResetCause()
+//clear RESC by SysCtl_clearResetCause(uint32_t)
+
 //External Reset
 //Power-On Reset 
 //Debugger Reset
@@ -13,4 +32,4 @@
 //Test Reset
 
 
-#endif /* 3_3_RESETS_ */
+#endif /* _3_3_RESETS_ */

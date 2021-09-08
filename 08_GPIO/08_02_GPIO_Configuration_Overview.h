45764afe517c987382a1b4cb523c0b4132b5b649
Mykhailo <48632891+MykhailoVoinikov@users.noreply.github.com>
2021-09-08 18:47:48 +0200

    gpio

--- 08_GPIO/08_02_GPIO_Configuration_Overview.h
+++ 08_GPIO/08_02_GPIO_Configuration_Overview.h
@@ -1,6 +1,18 @@
 #ifndef _8_2_GPIO_CONFIGURATION_OVERVIEW_H_
 #define _8_2_GPIO_CONFIGURATION_OVERVIEW_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_nmi.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_types.h"
+#include "cpu.h"
+#include "inc/hw_gpio.h"
+#include "debug.h"
+#include "interrupt.h"
+#include "sysctl.h"
+
 void _8_2_GPIO_Configuration_Overview (void);
 
 #endif /* _8_2_GPIO_CONFIGURATION_OVERVIEW_H_ */
